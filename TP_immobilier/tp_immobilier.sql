6a3371a77afb1ea7104f4dd0a935ba35595e550d
Cedric <cedric-lopez@hotmail.fr>
2021-03-04 14:57:05 +0100

    TP immobilier: Correction insert logement.superficie

--- TP_immobilier/tp_immobilier.sql
+++ TP_immobilier/tp_immobilier.sql
@@ -17,7 +17,7 @@ CALL insert_logement_agence;
 CALL insert_personne;
 CALL insert_logement_personne;
 CALL insert_demande;
-
+#TRUNCATE TABLE logement;
 			# Privilèges
 SELECT * FROM mysql.user;
 CREATE USER "afpa" IDENTIFIED BY "afpa";
@@ -37,23 +37,37 @@ SHOW GRANTS FOR 'cda314@localhost';
 
             
 			# Requetes SQL
-            
+
 #1. Affichez le nom des agences
-/*--------------------------ESSAI--------------------------------------------------------*/
-SELECT * FROM personne;
-
-
-/*--------------------------ESSAI--------------------------------------------------------*/
-
+SELECT nom FROM agence;
 #2. Affichez le numéro de l’agence « Orpi »
+SELECT idAgence FROM agence WHERE nom LIKE "orpi";
 #3. Affichez le premier enregistrement de la table logement
+SELECT * FROM logement ORDER BY idLogement LIMIT 1;
 #4. Affichez le nombre de logements (Alias : Nombre de logements)
+SELECT COUNT(*) AS "Nombre de logements" FROM logement;
 #5. Affichez les logements à vendre à moins de 150 000 € dans l’ordre croissant des prix.
+SELECT * FROM logement WHERE categorie = "vente" HAVING prix < 350000 ORDER BY prix;
 #6. Affichez le nombre de logements à la location (alias : nombre)
+SELECT COUNT(*) nombre FROM logement WHERE categorie LIKE "location";
 #7. Affichez les villes différentes recherchées par les personnes demandeuses d'un logement
+SELECT DISTINCT ville FROM demande;
+SELECT ville FROM demande GROUP BY ville;
 #8. Affichez le nombre de biens à vendre par ville
+SELECT ville, COUNT(*) FROM logement WHERE  categorie LIKE "vente" GROUP BY ville;
 #9. Quelles sont les id des logements destinés à la location ?
+SELECT idLogement FROM logement WHERE categorie LIKE "location";
+/*--------------------------En Cours--------------------------------------------------------*/
+SELECT * FROM agence;
+SELECT * FROM demande;
+SELECT * FROM logement;
+SELECT * FROM logement_agence;
+SELECT * FROM logement_personne;
+SELECT * FROM personne;
 #10. Quels sont les id des logements entre 20 et 30m² ?
+SELECT idLogement, superficie FROM logement WHERE superficie >= 20 AND superficie <= 30;
+/*--------------------------En cours--------------------------------------------------------*/
+
 #11. Quel est le prix vendeur (hors frais) du logement le moins cher à vendre ? (Alias : prix minimum)
 #12. Dans quelles villes se trouve les maisons à vendre ?
 #13. L’agence Orpi souhaite diminuer les frais qu’elle applique sur le logement ayant l'id « 3 ». Passer les frais de ce logement de 800 à 730€          
